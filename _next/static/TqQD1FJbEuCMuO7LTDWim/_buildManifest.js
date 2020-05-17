9e1e3426f3e759d700b92533ad4ff0be3ea0cf86
felipecesr <felipecesr@users.noreply.github.com>
2020-05-17 17:17:47 +0000

    deploy: 6bc04e6f70565719a9d4dc91894a181dc5470830

---
+++ _next/static/TqQD1FJbEuCMuO7LTDWim/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/[name]":[a],"/index":[a]}}("static\u002Fchunks\u002F1b4fd11786c692778117b8d2458094dd405f1cf6.61b3ff20f8db48002ad4.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
