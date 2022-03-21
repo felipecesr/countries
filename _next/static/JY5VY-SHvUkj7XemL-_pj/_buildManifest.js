2dbff3e82fcf2dc3d58b9f2e63fc1fc8f0fc1f8f
felipecesr <felipecesr@users.noreply.github.com>
2022-03-21 19:36:47 +0000

    deploy: fdff098b2a953178d1ded0e0942731eca41a6120

--- _next/static/JY5VY-SHvUkj7XemL-_pj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-88235f555ae6c4ef.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/[alpha]":["static/chunks/pages/[alpha]-62c10af0187da93e.js"],sortedPages:["/","/_app","/_error","/[alpha]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
