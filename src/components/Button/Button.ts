b3206d6623aae24cb0a913dcfbdafb7bdfc78f49
Felipe César <felipecesr@gmail.com>
2022-04-16 18:54:21 -0300

    refactor: button

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background-color: var(--color-elements);
+  border: 0;
+  border-radius: 5px;
+  color: var(--color-text);
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+  padding: 0.6rem 1.5rem;
+`
+
+export default Button
