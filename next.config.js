e87bed98dbfd6abf1ea29f2a8a1686bb476fd1c7
Felipe César <felipecesr@gmail.com>
2022-04-18 14:23:31 -0300

    chore: add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
 
 /**
  * @type {import('next').NextConfig}
@@ -10,4 +13,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
